d37ae246d7ed17922986dfaa304335275c02566c
Samuel Adekunle <me@samuel-adekunle.software>
2021-02-05 07:56:33 +0000

    fix: install favicon
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Routh Table Generator</title>
           <meta name="title" content="Routh Table Generator" />
           <meta
             name="description"
@@ -43,6 +42,25 @@ class MyDocument extends Document {
             property="twitter:image"
             content="https://routhtable.netlify.app/meta.jpg"
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
