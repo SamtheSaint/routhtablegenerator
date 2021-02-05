3b1c374b01b592cf479915a6f653f86fa75fda80
Samuel Adekunle <me@samuel-adekunle.software>
2021-02-05 07:39:52 +0000

    feat: added explanation and equation (v2)
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ import {
   Toolbar,
   Typography
 } from '@material-ui/core';
-import { useEffect, useRef, useState } from 'react';
+import { StrictMode, useEffect, useRef, useState } from 'react';
+import MathJax from 'react-mathjax';
 import Matrix from '../components/react-matrix';
 import { generateRouthTable } from '../lib/routh';
 
@@ -44,6 +45,12 @@ function App() {
     return __columns;
   };
 
+  const generateEquation = () => {
+    const N = coefficients.length;
+    const equation = coefficients.map((v, i) => `${v}s^${N - i}`).join(' + ');
+    return equation;
+  };
+
   useEffect(() => {
     if (outputMatrixRef.current) {
       outputMatrixRef.current.setState({
@@ -53,44 +60,68 @@ function App() {
   }, [routhTable]);
 
   return (
-    <Container maxWidth="md">
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="h3" color="inherit">
-            Generate Routh Table
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box style={{ margin: '24px 0' }}>
-        <Typography variant="h4">
-          Enter coefficients of the characteristic polynomial:
-        </Typography>
-        <Grid container>
-          <Matrix
-            columns={initialColumns}
-            resize="horizontal"
-            ref={inputMatrixRef}
-          />
-          <Button onClick={handleClick} style={{ marginLeft: 8 }}>
-            <Typography variant="button">Submit</Typography>
-          </Button>
-        </Grid>
-      </Box>
-      <Box>
-        {routhTable && (
-          <>
+    <StrictMode>
+      <MathJax.Provider>
+        <Container maxWidth="md">
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <Typography variant="h3" color="inherit">
+                Generate Routh Table
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Box style={{ margin: '16px 0' }}>
             <Typography variant="h4">
-              Routh Table for coefficients: [{coefficients.join(' ')}]
+              Routh-Hurwitz Stability Criterion:
             </Typography>
-            <Matrix
-              ref={outputMatrixRef}
-              columns={routhToColumns(routhTable)}
-              readonly={true}
-            />
-          </>
-        )}
-      </Box>
-    </Container>
+            <Typography variant="body1" style={{ padding: '8px 0' }}>
+              In control system theory, the Routh–Hurwitz stability criterion is
+              a mathematical test that is a necessary and sufficient condition
+              for the stability of a linear time invariant (LTI) control
+              system&nbsp;
+              <a href="https://en.wikipedia.org/wiki/Routh%E2%80%93Hurwitz_stability_criterion">
+                (Wikipedia).
+              </a>
+            </Typography>
+            <Typography variant="body1">
+              In summary, if there is no sign change in the first column of the
+              Routh table then the characteristic polynomial describes an
+              asymptotic stable system.
+            </Typography>
+          </Box>
+          <Box style={{ margin: '16px 0' }}>
+            <Typography variant="h4">
+              Enter the coefficients of the characteristic polynomial:
+            </Typography>
+            <Grid container>
+              <Matrix
+                columns={initialColumns}
+                resize="horizontal"
+                ref={inputMatrixRef}
+              />
+              <Button onClick={handleClick} style={{ marginLeft: 8 }}>
+                <Typography variant="button">Submit</Typography>
+              </Button>
+            </Grid>
+          </Box>
+          <Box style={{ margin: '16px 0' }}>
+            {routhTable && (
+              <>
+                <Typography variant="h4">
+                  Routh Table for polynomial:{' '}
+                  <MathJax.Node inline formula={generateEquation()} />
+                </Typography>
+                <Matrix
+                  ref={outputMatrixRef}
+                  columns={routhToColumns(routhTable)}
+                  readonly={true}
+                />
+              </>
+            )}
+          </Box>
+        </Container>
+      </MathJax.Provider>
+    </StrictMode>
   );
 }
 
